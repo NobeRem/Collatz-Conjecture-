9a0108b902a09593152db8df797a01f4e6bb9a9d
NobeRem <114718917+NobeRem@users.noreply.github.com>
2023-02-16 14:37:19 -0600

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,20 @@
+def collatz(n):
+
+	while n > 1:
+	
+		if n % 2 ==0:
+			n = n/2 
+			print(n)
+			
+
+		elif n % 2 != 0:
+			n = n * 3 + 1
+			print(n)
+			
+		else:
+			n == 0
+			print(n)
+			break
+
+n = int(input('Number to be used: '))
+collatz(n)
